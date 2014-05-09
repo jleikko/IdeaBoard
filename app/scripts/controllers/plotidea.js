97937e14918f4bc9c3f5f81fb14a70bd6b186f6d
Jaakko Leikko <jaakko.leikko@haaga-helia.fi>
2014-05-09 15:36:42 +0300

    ideasearch developed

--- app/scripts/controllers/plotidea.js
+++ app/scripts/controllers/plotidea.js
@@ -1,16 +1,28 @@
 'use strict';
 
 angular.module('ideaBoardApp')
-  .controller('PlotIdeaCtrl', function ($scope, $http) {
-
-        $http.get('static_json/suggestions.json').success(function(data) {
-            $scope.ideas = data;
-            $scope.sidea = $scope.ideas[0];
-        });
+    .controller('PlotIdeaCtrl', function ($scope, $http, $filter) {
 
         $scope.topIdeaLimit = 7;
+        $scope.ideasearch = '';
+        $scope.ideas = [];
 
-        $scope.showIdea = function(sidea) {
+
+
+        $http.get('static_json/suggestions.json').success(function (data) {
+            $scope.ideas = data;
+            $scope.sidea = $scope.ideas[0];
+
+        });
+
+
+
+        $scope.showIdea = function (sidea) {
             $scope.sidea = sidea;
-        }
-  });
+        };
+
+
+        $scope.searchChange = function () {
+            $scope.sidea = $filter('filter')($scope.ideas, $scope.ideasearch)[0];
+        };
+    });
