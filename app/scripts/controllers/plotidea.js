73290a1bc423e7056857c685c608562c2397f1ac
Jaakko Leikko <jaakko.leikko@haaga-helia.fi>
2014-05-09 14:19:50 +0300

    developing read site further

--- app/scripts/controllers/plotidea.js
+++ app/scripts/controllers/plotidea.js
@@ -1,7 +1,16 @@
 'use strict';
 
 angular.module('ideaBoardApp')
-  .controller('PlotIdeaCtrl', function ($scope) {
+  .controller('PlotIdeaCtrl', function ($scope, $http) {
 
+        $http.get('static_json/suggestions.json').success(function(data) {
+            $scope.ideas = data;
+            $scope.sidea = $scope.ideas[0];
+        });
 
+        $scope.topIdeaLimit = 7;
+
+        $scope.showIdea = function(sidea) {
+            $scope.sidea = sidea;
+        }
   });
